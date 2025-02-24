9f95cd03de7f01956e240d9cb15da3f21af84b26
Damith Harshana <damith.dhlr@hotmail.com>
2025-02-24 21:56:23 +0530

    finish script file

---
+++ script.js
@@ -0,0 +1,25 @@
+function appendValue(value) {
+    let display = document.getElementById("display");
+    if (display.value === "0") {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+function clearDisplay() {
+    document.getElementById("display").value = "0";
+}
+function deleteLast() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+    if (display.value === "") {
+        display.value = "0";
+    }
+}
+function calculate() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch {
+        alert("Invalid Input");
+    }
+}
